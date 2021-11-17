a14666f47cc239a4be87b6832b188fc05671f281
yahyatarekk <yahyatarek420@gmail.com>
2021-11-17 22:08:51 +0200

    task2

---
+++ print your name.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main(List<String> arguments){
+   
+    for(int i=1;i<=100;i+=2){     //print you name from 1 to 100 (odd numbers only)
+      print("$i->yahya");
+    }
+}
